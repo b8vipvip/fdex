de27cd465c850a1289a270af259f4b02442752b5
b8vipvip <917931227@qq.com>
2026-08-15 13:14:20 +0800

    feat: add websocket and image rendering dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
