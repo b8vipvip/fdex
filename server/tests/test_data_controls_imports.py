92707216b0dd6f5e96767f2d77620371a5969320
b8vipvip <917931227@qq.com>
2026-08-25 10:36:13 +0800

    Remove redundant import smoke test

--- server/tests/test_data_controls_imports.py
+++
@@ -1,4 +0,0 @@
-def test_phase7_3_modules_import() -> None:
-    import app.account_data_export  # noqa: F401
-    import app.account_operations  # noqa: F401
-    import app.memory_scope_registry  # noqa: F401
