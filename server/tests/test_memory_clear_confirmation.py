000bc73c5371ded29ed76cbd38d3d8f937a4bdc0
b8vipvip <917931227@qq.com>
2026-08-25 10:36:22 +0800

    Remove duplicate confirmation model test

--- server/tests/test_memory_clear_confirmation.py
+++
@@ -1,6 +0,0 @@
-from app.auth_routes import ClearMemoryRequest
-
-
-def test_clear_memory_confirmation_phrase() -> None:
-    request = ClearMemoryRequest(password="abcdefgh", confirmation="CLEAR MY FDEX MEMORY")
-    assert request.confirmation == "CLEAR MY FDEX MEMORY"
